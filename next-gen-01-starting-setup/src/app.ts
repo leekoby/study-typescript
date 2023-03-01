3cf1623b5e4e4c410df4f1c4e62df9d8ce0b50d2
leekoby <abc2752@naver.com>
2023-03-01 12:43:56 +0900

    Update app.ts

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,17 @@
 // Code goes here!
+
+function oneMore(){
+    console.log('one more')
+}
+function run(){
+    console.log('run run');
+    setTimeout(()=>{
+        console.log('wow')},0)
+
+new Promise((resolve)=>{
+    resolve('hi');
+})
+.then(console.log);
+oneMore();
+}
+setTimeout(run,5000);
